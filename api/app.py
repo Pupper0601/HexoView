22336fe148651da39c9963fecd6283bda923f15e
Pupper0601 <pupper.cheng@gmail.com>
2023-09-16 09:13:22 +0800

    解决跨域问题

--- api/app.py
+++ api/app.py
@@ -1,7 +1,8 @@
 import uvicorn
 from handle_db import SqliteHandle
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 sq = SqliteHandle()
@@ -13,15 +14,48 @@ class Item(BaseModel):
     view: str
 
 
+# 添加CORS中间件配置
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:5500",  # 添加你需要的源
+    "https://hexo-view.vercel.app",  # 这是你的后端服务的源
+    "https://hexo-view.pupper.cn",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/read/")
 async def read_view(address: str):
     return sq.find_view({"address": address})
 
 
 @app.post("/insert/")
-async def insert_view(item: Item):
+async def insert_view(item: Item, response: Response):
     address = item.address
     ip = item.ip
     view = item.view
 
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+
     return sq.insert_view({"address": address, "ip": ip, "view": view})
+
+
+@app.options("/{anything:path}")
+async def options(anything: str):
+    return Response(headers={
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "*",
+    })
